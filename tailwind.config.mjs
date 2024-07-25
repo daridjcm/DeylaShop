2167136202db635df54c433b3c790fc3234002f7
Daridjcm <daricorreamedina@gmail.com>
2024-07-25 18:08:55 -0500

    Add Yesicon and improvement responsive design

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,5 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme')
+const { addDynamicIconSelectors } = require('@iconify/tailwind')
 
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
@@ -7,8 +8,24 @@ export default {
 		extend: {
 			fontFamily: {
 				'sans': ['"Montserrat", sans-serif', ...defaultTheme.fontFamily.sans]
-			}
+			},
+			backgroundImage: {
+				'radial-gradient': 'radial-gradient(var(--tw-gradient-stops))',
+			},
+			animation: {
+				bounce: 'bounce 0.6s ease infinite',
+			  },
+			  keyframes: {
+				bounce: {
+				  '0%, 20%, 50%, 80%, 100%': { transform: 'translateY(0)' },
+				  '40%': { transform: 'translateY(-20px)' },
+				  '60%': { transform: 'translateY(-10px)' },
+				},
+			  },
+			  colors: {
+				'btn-bg': '#fff',
+			},
 		},
 	},
-	plugins: [],
+	plugins: [addDynamicIconSelectors()]
 }
