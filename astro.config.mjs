424a41c17f177b1602aeadd7393c76502bcd3b27
daridjcm <cdarianamedina@gmail.com>
2025-05-22 20:28:08 -0500

    Add redirect to cartpage

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import react from '@astrojs/react';
+import node from '@astrojs/node';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react({experimentalReactChildren: true})],
+  
+  adapter: node({
+    mode: 'standalone'
+  })
 });
